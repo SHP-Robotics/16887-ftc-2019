e94438154c75d6496052342bf41839ee9aa6406e
PaulFong1 <55032544+PaulFong1@users.noreply.github.com>
2019-10-22 12:29:55 -0700

    Update MainTeleOp.java

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -29,47 +29,5 @@ public class MainTeleOp extends BaseRobot {
         //drive train
         tankanum_drive(gamepad1.right_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
 
-        //climber
-        if(gamepad1.dpad_up) {
-            climb(-1);
-        } else if (gamepad1.dpad_down) {
-            climb(1);
-        } else {
-            climb(0);
-        }
-
-        //flip
-        if(gamepad1.a || gamepad1.dpad_right) {
-            flip(-1);
-        } else if (gamepad1.b) {
-            flip(1);
-        } else {
-            flip(0);
-        }
-
-        //intake servo
-        if(gamepad1.left_bumper) {
-            set_intake_servo(ConstantVariables.K_INTAKE_SERVO_IN);
-        } else if (gamepad1.right_bumper){
-            set_intake_servo(ConstantVariables.K_INTAKE_SERVO_OUT);
-        }
-
-        //lift
-        if(gamepad1.dpad_right) {
-            lift(-1);
-        } else if (gamepad1.dpad_left) {
-            lift(1);
-        } else {
-            lift(0);
-        }
-
-        //bucket
-        if(gamepad1.x) {
-            bucket(-1);
-        } else if (gamepad1.y) {
-            bucket(1);
-        } else {
-            bucket(0);
-        }
-    }
+            }
 }
